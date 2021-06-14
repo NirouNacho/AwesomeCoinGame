af063ecc5410e866adbdefec9cd8f05cabe1196b
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-14 01:17:29 -0500

    bunny jumping

--- Awesome Coin Game/Assets/Scripts/PlayerController.cs
+++ Awesome Coin Game/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    // variables
+
+    private Rigidbody2D rigidBody;
+    public float thrust = 10.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            print(" Left mouse's button clicked");
+        if (Input.GetMouseButtonDown(0)
+            || Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.W)   ) 
+        {
+
+            Jump();
         }
+    }
+
+    //Methods
+
+    void Jump() {
+        rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+
     }
 }
