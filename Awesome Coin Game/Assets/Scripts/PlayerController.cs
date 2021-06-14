a5409b15e9a108f5d711087d87fa4f2acbaf3e80
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-14 02:28:17 -0500

    jumpbuny
    transition into animation running and jumping

--- Awesome Coin Game/Assets/Scripts/PlayerController.cs
+++ Awesome Coin Game/Assets/Scripts/PlayerController.cs
@@ -10,38 +10,45 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidBody;
     public float thrust = 10.0f;
     public LayerMask groundLayerMask;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator.SetBool("isAlive", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)
-            || Input.GetKeyDown(KeyCode.Space)
-            || Input.GetKeyDown(KeyCode.W)   ) 
-        {
+        bool isOnTheGround = IsOnTheGround();
+        animator.SetBool("isGrounded",isOnTheGround);
 
+        if ((Input.GetMouseButtonDown(0)
+            || Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.W))
+            && isOnTheGround)
+        {    
             Jump();
         }
+
     }
 
     //Methods
 
     void Jump() {
-        if (IsOnTheGround())
-        {
+       
             rigidBody.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
-        }
+        
     }
 
     bool IsOnTheGround() {
         print("mask=" + groundLayerMask);
-        return Physics2D.Raycast(this.transform.position,
-            Vector2.down,1.5f,groundLayerMask.value);
-        
+        return Physics2D.Raycast(
+            this.transform.position,
+            Vector2.down,
+            1.5f, 
+            groundLayerMask.value);
     }
 }
