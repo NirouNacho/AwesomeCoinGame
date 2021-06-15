397676743d42ab599e708651e339fbe1324cb8c9
NicoRosero <nicolas.nacho93@gmail.com>
2021-06-15 00:10:56 -0500

    GameManager
    added structure of game logic

--- Awesome Coin Game/Assets/Scripts/GameManager.cs
+++ Awesome Coin Game/Assets/Scripts/GameManager.cs
@@ -22,14 +22,25 @@ Sabado,
 Domingo
 }
 
+public enum GameState {
+    Menu, InGame, GameOver, Pause
+} 
+
 public class GameManager : MonoBehaviour
 {
-    DaysOfTheWeek currentDay = DaysOfTheWeek.Domingo;
+    //Starts pur game
+    // DaysOfTheWeek currentDay = DaysOfTheWeek.Domingo;
+
+    GameState currentGameState = GameState.Menu;
+
 
     // Starts our game
     public void StartGame()
     {
-        print("Hoy es " + (int)currentDay);
+        // print("Hoy es " + (int)currentDay);
+
+        ChangeGameState(GameState.InGame);
+
     }
 
     private void Start()
@@ -40,11 +51,31 @@ public class GameManager : MonoBehaviour
     // Called when palyer dies
     public void GameOver()
     {
-        
+        ChangeGameState(GameState.GameOver);
     }
     //Called when player quit the game and go to the main menu
     public void BackToMainMenu() 
-    { 
+    {
+        ChangeGameState(GameState.Menu);
+    }
+
+    private void ChangeGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.Menu)
+        {
+            //Let show the menu scene
+        }
+        else if (newGameState == GameState.InGame)
+        {
+            //Unity must show the Real Game
+        }
+        else if (newGameState == GameState.GameOver) 
+        {
+            // Lets load end of the game scene
+        }
+
+        currentGameState = newGameState;
 
     }
+
 }
